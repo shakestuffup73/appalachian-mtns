e87453e64df93c1c710a0a02943b1da8262377a4
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-03 22:56:58 -0400

    write gear index view checklist

--- models/gear.js
+++ models/gear.js
@@ -11,9 +11,8 @@ const gearSchema = new Schema ({
   topRopeOnRock: Boolean,
   fullIceRack: Boolean,
   fullRockRack: Boolean,
-  petzlScrews: String,
-  blackDiamondScrews: String,
-  otherScrews: String,
+  rockRack: String,
+  iceRack: String,
   dryRope60m: Boolean,
   dryRope70m: Boolean,
   halfRopes: Boolean,
@@ -22,6 +21,9 @@ const gearSchema = new Schema ({
   helmet: Boolean,
   harness: Boolean,
   otherGear: String,
+  missingRock: String,
+  missingIce: String,
+  extraGear: Boolean,
 })
 
 const Gear = mongoose.model('Gear', gearSchema)
